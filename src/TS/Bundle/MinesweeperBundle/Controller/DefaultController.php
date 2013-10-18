a541a4385aa416e343ac0ad46da1ad0a780b3754
Roger Clotet <roger.clotet@socialpoint.es>
2013-10-18 22:11:36 +0200

    First example of chat working

--- src/TS/Bundle/MinesweeperBundle/Controller/DefaultController.php
+++ src/TS/Bundle/MinesweeperBundle/Controller/DefaultController.php
@@ -3,23 +3,40 @@
 namespace TS\Bundle\MinesweeperBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DefaultController extends Controller
 {
+    /**
+     * @Route("/")
+     * @Method("GET")
+     * @Template()
+     */
     public function indexAction()
     {
+        $randId = mt_rand();
+
         $globals = array(
             'user' => array(
-                'name' => 'Provisional User',
-                'username' => 'provuser',
+                'name' => 'User ' . $randId,
+                'username' => 'provuser' . $randId,
             )
         );
 
-        return $this->render(
-            'TSMinesweeperBundle:Default:index.html.twig',
-            array(
-                'globals' => $globals
-            )
-        );
+        return array('globals' => $globals);
+    }
+
+    /**
+     * @Route("/match/{matchId}/chat")
+     * @Method("GET")
+     * @Template()
+     */
+    public function chatAction()
+    {
+        $chat = 'This is the chat and now it\'s ' . time() . '!';
+
+        return array('chat' => $chat);
     }
 }
--- src/TS/Bundle/MinesweeperBundle/Resources/public/js/game.js
+++ src/TS/Bundle/MinesweeperBundle/Resources/public/js/game.js
@@ -1,6 +1,16 @@
 'use strict';
 
+var pollingRate = 1000;
+
 $(document).ready(function () {
+    setInterval(function () {
+        $.get('/match/0/chat', function (data) {
+            var chat = $('#chat');
+
+            chat.html(data).scrollTop(chat[0].scrollHeight);
+        });
+    }, pollingRate);
+
     $('.board-cell').click(function () {
         console.log('Click (' + $(this).data('row') + ', ' + $(this).data('col') + ')');
     });
