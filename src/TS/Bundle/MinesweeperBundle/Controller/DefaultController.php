6a320762d43d1d33a729bede1591f30b8b537ff4
Roger Clotet <roger.clotet@socialpoint.es>
2013-10-20 23:11:28 +0200

    Removed unnecessary use statements

--- src/TS/Bundle/MinesweeperBundle/Controller/DefaultController.php
+++ src/TS/Bundle/MinesweeperBundle/Controller/DefaultController.php
@@ -3,11 +3,9 @@
 namespace TS\Bundle\MinesweeperBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use TS\Bundle\MinesweeperBundle\Entity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
